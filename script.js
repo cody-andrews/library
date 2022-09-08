95ac3ca8b9a0f14247d3927e0a770b6a82ce11c7
Cody Andrews <cera117@gmail.com>
2022-09-07 22:37:43 -0700

    updated css styling and javascript to add books via form then clear form

--- script.js
+++ script.js
@@ -1,26 +1,74 @@
+//DOM Objects
+
+const submit = document.querySelector('#submitBook');
+
+let haveRead = '';
+
+let myLibrary = [];
+
+const theHobbit = new Books('The Hobbit', 'J.R.R. Tolkien', '295 Pages', 'yes')
+
 const books = {
     bookName: "",
     author: "",
-    numPages: ""
+    numPages: "",
+    haveRead: ''
   }
 
-  function Books (bookName, author, numPages, haveRead)   {
-    this.bookName = bookName; 
+  function Books (title, author, numPages, haveRead)   {
+    this.title = title; 
     this.author = author; 
     this.numPages = numPages;
     this.haveRead = haveRead;
-
-    if (haveRead === 'yes'){
-        haveRead = "Have read"
-    }
-       else
-        haveRead = 'Have not read'
-    
-        this.info = function(){
-        return (bookName + ", " + author + ", " + numPages + ', ' + haveRead)
-    }
   }
 
-  const theHobbit = new Books('The Hobbit', 'J.R.R. Tolkien', '295 Pages', 'yes')
 
-  console.log(theHobbit.info());
+
+  function addBookToLibrary(book)  {
+    
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;    
+
+      if (document.getElementById('haveRead').checked){
+        haveRead = "Yes";
+      }
+      else{
+        haveRead = 'No'
+      }
+    
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.haveRead = haveRead
+ 
+  
+    let newBook = new Books(title, author, pages, haveRead)
+
+
+    myLibrary.push(newBook)
+    myLibrary.push(theHobbit)
+    console.log(title.value)
+    for (let i = 0; i < myLibrary.length; i++) {
+      console.log(myLibrary[i]);
+  }    
+    clear()
+  }
+
+   function clear()  {
+    document.getElementById('myForm').reset();
+   }
+
+  submit.addEventListener("click", addBookToLibrary);
+  // addBookToLibrary(theHobbit);
+
+  // console.log(myLibrary[0]);
+
+  function openForm() {
+    document.getElementById("bookFormPopup").style.setProperty('visibility', 'visible', 'important');
+  }
+  
+  function closeForm() {
+    document.getElementById("bookFormPopup").style.setProperty('visibility', 'hidden', 'important');
+  }
+
